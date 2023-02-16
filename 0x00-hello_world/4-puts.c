5500ce493a82c356a3520583b8af89a219fdb303
pathogneumonic <godwinchiazor@gmail.com>
2023-02-16 14:33:44 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ *main entry: "Programming is like building a multilingual puzzle
+ return 0*/
 
 int main(void) {
 	puts("\"Programming is like building a multilingual puzzle"/n);
