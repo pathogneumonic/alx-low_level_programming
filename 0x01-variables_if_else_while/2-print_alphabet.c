2aa78bbefe6a4339bb826664671f967f11829302
pathogneumonic <godwinchiazor@gmail.com>
2023-02-20 15:58:02 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: a program that prints alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= z, ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
