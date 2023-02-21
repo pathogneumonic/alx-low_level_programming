b20853eeac75edfa894458595ca02b57613d64a1
pathogneumonic <godwinchiazor@gmail.com>
2023-02-20 16:53:14 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Description: print alphabets except q and e
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	if (ch != e && ch != q)
+		putchar (ch);
+}
+else
+{
+}
+putchar (\n);
+return (0);
+}
