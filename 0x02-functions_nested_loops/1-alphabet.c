b4d3d1f7c4382367096425445840a5372fb28b52
pathogneumonic <godwinchiazor@gmail.com>
2023-02-22 15:47:09 -0800

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  * Return: 0 always
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
 
-	_putchar('/n');
+	_putchar('\n');
 	return (0);
 }
