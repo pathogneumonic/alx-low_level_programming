84081199046c7541725bfe60efebb8fa85a358aa
pathogneumonic <godwinchiazor@gmail.com>
2023-02-22 15:24:29 -0800

    is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * islower - Write a function that checks for lowercase character
+ * Return: 1 if lowercase otherwise 0
+ */
+
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i == 'c')
+		{
+		return (1);
+		}
+		else
+		{
+		return (0);
+		}
+}
