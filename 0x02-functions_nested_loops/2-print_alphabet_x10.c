dd4f1a808e7bd59a80962fbf7dd1b9dd3fcc59a6
pathogneumonic <godwinchiazor@gmail.com>
2023-02-22 18:19:29 -0800

    print 10 alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,10 @@ void print_alphabet_x10(void)
 	char x;
 	int y;
 
-	for (y = 0; y <= 10; y++)
+	for (y = 0; y < 10; y++)
 	{
 		for (x = 'a'; x <= 'z'; x++)
 			_putchar(x);
 		_putchar('\n');
 	}
-	return (0);
 }
