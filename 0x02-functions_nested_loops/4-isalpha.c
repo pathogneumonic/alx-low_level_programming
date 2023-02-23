4ecb8a6aee3f964ad8cc9fae75930bb87874a18a
pathogneumonic <godwinchiazor@gmail.com>
2023-02-22 16:14:29 -0800

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * isalpha - Write a function that checks for alphabetic character
+ * Return: 1 success 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	char lower, upper;
+	int isletter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper  = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+			{
+				isletter = 1;
+			}
+		}
+	}
+	return (isletter);
+}
