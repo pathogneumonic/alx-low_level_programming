d4a8ef37e7e526101f4ee000d07d21e75186e301
pathogneumonic <godwinchiazor@gmail.com>
2023-02-24 14:49:00 -0800

    print 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 /**
  * print_numbers - prints numbers from 0 to 9 with a new line
  *
- * Return: 0
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i);
 	}
