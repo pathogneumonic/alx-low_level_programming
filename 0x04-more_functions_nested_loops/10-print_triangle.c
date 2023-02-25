c5d77a634a04ff4cad034af2f06b86f6cc7d3b79
pathogneumonic <godwinchiazor@gmail.com>
2023-02-24 19:25:18 -0800

    print traingle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: argument
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - 1); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar('#');
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
