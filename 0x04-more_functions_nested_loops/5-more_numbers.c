da7cc03736c5a69db3ef987e57b6a6ec9a65f6ed
pathogneumonic <godwinchiazor@gmail.com>
2023-02-24 16:31:56 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i <= 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('1' + (j % 10));
+			}
+			_putchar('0' + j);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
