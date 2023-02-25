22ff90a90a9c49ff2b4f2864625674fe6cec4ee2
pathogneumonic <godwinchiazor@gmail.com>
2023-02-24 18:43:26 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - FizzBuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int b = 1;
+
+	while (b <= 100)
+	{
+		if ((b % 3 == 0) && (b % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (b % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (b % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d\n", b);
+		}
+
+		b++;
+	}
+	return (0);
+}
