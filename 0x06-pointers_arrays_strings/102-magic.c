2b0fc89d3c906719061f21a41dd84e2443b5d99b
pathogneumonic <godwinchiazor@gmail.com>
2023-03-02 17:03:50 -0800

    print a line of code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,11 +14,13 @@ int main(void)
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
-	 * - you are not allowed to code anything other than this line of code
+	 * - you are not allowed to code anything other than this line
+	 *   of code.
 	 */
 
-	p[5] = 98;
+	*(p + 5) = 98;
 	/* ... this prints 98\n */
+
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
