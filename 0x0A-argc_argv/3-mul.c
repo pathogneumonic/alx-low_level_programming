f4a9d8ca835eaabd3ae937ca3f9511f364528b21
pathogneumonic <godwinchiazor@gmail.com>
2023-03-15 16:26:28 -0700

    number of coins

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
